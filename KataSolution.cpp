a2056ff1c8d87a1e7d52f2de9d848292b97f8a74
Mariah <48637409+mdiazXV@users.noreply.github.com>
2024-06-12 07:32:32 -0400

    Add files via upload
    Added solution for problem in which function takes integer argument and adds from 1 through the input and returns the sum.

--- KataSolution.cpp
+++ KataSolution.cpp
@@ -23,3 +23,25 @@ int goals (int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
   sum = A + B + C;
   return sum;
 }
+
+
+/*Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0. 
+Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result 
+and it's not part of it, see the sample tests.
+
+For example (Input -> Output):
+
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)*/
+
+int summation(int num){
+  int x=1 ;
+  int y = num ;
+  int sum=0;
+  
+  for (x=1; x<=y; x++){
+    sum+=x;
+  }
+  
+  return sum ;
+}
